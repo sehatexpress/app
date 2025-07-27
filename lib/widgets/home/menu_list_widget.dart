d339c4bf322ad05cc42d74085f4cdd4795ed56b4
Ayub Alam <alamayub85@gmail.com>
2025-07-27 18:07:26 +0530

    product menu card

--- lib/widgets/home/menu_list_widget.dart
+++ lib/widgets/home/menu_list_widget.dart
@@ -4,6 +4,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../config/constants.dart';
 import '../../config/extensions.dart';
 import '../../providers/value_provider.dart';
+import '../generic/custom_image_provider.dart';
 import '../generic/loader_widget.dart';
 
 class MenuListWidget extends ConsumerWidget {
@@ -35,7 +36,69 @@ class MenuListWidget extends ConsumerWidget {
           clipBehavior: Clip.antiAlias,
           child: Column(
             children: [
-              Container(height: 200, color: ColorConstants.primary),
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: CustomImageProvider(
+                        image:
+                            'https://media.istockphoto.com/id/1457979959/photo/snack-junk-fast-food-on-table-in-restaurant-soup-sauce-ornament-grill-hamburger-french-fries.jpg?s=612x612&w=0&k=20&c=QbFk2SfDb-7oK5Wo9dKmzFGNoi-h8HVEdOYWZbIjffo=',
+                      ),
+                    ),
+                    Positioned(
+                      top: 8,
+                      left: 8,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: ColorConstants.secondary,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 8,
+                          vertical: 6,
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.star_outline,
+                              size: 13,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(width: 2),
+                            Text(
+                              '4.6',
+                              style: context.text.labelSmall?.copyWith(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 8,
+                      right: 8,
+                      child: SizedBox(
+                        width: 32,
+                        height: 32,
+                        child: IconButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            backgroundColor: ColorConstants.primary,
+                            padding: EdgeInsets.zero,
+                          ),
+                          icon: const Icon(
+                            Icons.favorite_outline,
+                            size: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Padding(
                 padding: EdgeInsetsGeometry.all(16),
                 child: Column(
@@ -62,6 +125,74 @@ class MenuListWidget extends ConsumerWidget {
                       'This is the description of the the menu item which will be shown to the user',
                       style: context.text.bodySmall,
                     ),
+                    const SizedBox(height: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        border: Border.all(
+                          color: ColorConstants.secondary.withAlpha(28),
+                        ),
+                        color: ColorConstants.secondary.withAlpha(10),
+                      ),
+                      padding: EdgeInsets.all(12),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.bolt_outlined,
+                                color: ColorConstants.secondary,
+                              ),
+                              const SizedBox(width: 6),
+                              Text(
+                                'Nutrition per serving',
+                                style: context.text.bodySmall,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 6),
+                          GridView.count(
+                            primary: false,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            crossAxisCount: 2,
+                            padding: EdgeInsets.zero,
+                            childAspectRatio:
+                                ((context.screenWidth - 88) / 2) / 15,
+                            mainAxisSpacing: 6,
+                            crossAxisSpacing: 6,
+                            children: List.generate(
+                              _lists.length,
+                              (i) => Text(
+                                _lists[i],
+                                style: context.text.labelSmall,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          fixedSize: Size(double.infinity, 40),
+                          backgroundColor: ColorConstants.secondary,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        label: Text(
+                          'Add to Cart',
+                          style: context.text.labelLarge?.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                        icon: Icon(Icons.add, color: Colors.white),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -75,3 +206,10 @@ class MenuListWidget extends ConsumerWidget {
     );
   }
 }
+
+List<String> _lists = [
+  'Calories: 350',
+  'Protein: 35g',
+  'Carbs: 20g',
+  'Fat: 8g',
+];
--- lib/widgets/root/custom_appbar.dart
+++ lib/widgets/root/custom_appbar.dart
@@ -4,7 +4,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../config/typo_config.dart' show typoConfig;
 import '../../helper/helper.dart';
 import '../../providers/location_provider.dart' show locationProvider;
-import '../inputs/select_city_widget.dart';
 import 'location_selection_widget.dart';
 
 class CustomAppBar extends ConsumerWidget implements PreferredSizeWidget {
@@ -20,45 +19,39 @@ class CustomAppBar extends ConsumerWidget implements PreferredSizeWidget {
 
     return AppBar(
       titleSpacing: 16,
-      title: city != null
-          ? address != null
-                ? InkWell(
-                    onTap: () {
-                      genericDialogTransitionForTopBar(
-                        context: context,
-                        widget: SafeArea(
-                          bottom: false,
-                          child: const LocationSelectionWidget(),
-                        ),
-                      );
-                    },
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Icon(
-                          Icons.location_on,
-                          color: Colors.white,
-                          size: 24,
-                        ),
-                        const SizedBox(width: 4),
-                        Expanded(
-                          child: Text(
-                            '${address.city}\n${address.displayName}',
-                            style: typoConfig.textStyle.smallCaptionSubtitle2
-                                .copyWith(color: Colors.white),
-                          ),
-                        ),
-                        const SizedBox(width: 4),
-                        const Icon(
-                          Icons.keyboard_arrow_down,
-                          color: Colors.white,
-                          size: 24,
-                        ),
-                      ],
+      title: city != null && address != null
+          ? InkWell(
+              onTap: () {
+                genericDialogTransitionForTopBar(
+                  context: context,
+                  widget: SafeArea(
+                    bottom: false,
+                    child: const LocationSelectionWidget(),
+                  ),
+                );
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.location_on, color: Colors.white, size: 24),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: Text(
+                      '${address.city}\n${address.displayName}',
+                      style: typoConfig.textStyle.smallCaptionSubtitle2
+                          .copyWith(color: Colors.white),
                     ),
-                  )
-                : const SelectCityWidget()
-          : const SelectCityWidget(),
+                  ),
+                  const SizedBox(width: 4),
+                  const Icon(
+                    Icons.keyboard_arrow_down,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                ],
+              ),
+            )
+          : const SizedBox(),
     );
   }
 }
